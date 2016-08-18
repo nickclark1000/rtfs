4a8659c0b3d457f34b1ebee71819f97f80167297
Clark <nickclark1000@gmail.com>
2016-08-17 21:11:57 -0300

    updates

--- R/api_request_handler.R
+++ R/api_request_handler.R
@@ -1,23 +1,52 @@
 api_get <- function(path) {
     url <- modify_url("http://elite-tfsapp.elitecorp.com:8080", path = path)
     cat("Request URL:", url, "\n")
-    resp <- GET(url, authenticate("TEN\\U6033371", "Monday.1234567", type = "ntlm"))
+    resp <- GET(url, authenticate(username, pwd, type = "ntlm"))
 
     if (http_type(resp) != "application/json") {
         stop("API did not return json", call. = FALSE)
     }
 
-    jsonlite::fromJSON(content(resp, "text"))
+    parsed <- jsonlite::fromJSON(content(resp, "text"))
+
+    structure(
+      list(
+        content = parsed,
+        path = path,
+        response = resp
+      ),
+      class = "api_get"
+    )
+}
+
+print.api_get <- function(x, ...) {
+  cat("<TFS get", x$path, ">\n", sep = "")
+  str(x$content)
+  invisible(x)
 }
 
 api_post <- function(path, query) {
     url <- modify_url("http://elite-tfsapp.elitecorp.com:8080", path = path)
     cat("Request URL:", url, "\n")
-    resp <- POST(url, authenticate("TEN\\U6033371", "Monday.1234567", type = "ntlm"), body = list(query = query), encode = "json")
+    resp <- POST(url, authenticate(username, pwd, type = "ntlm"), body = list(query = query), encode = "json")
     if (http_type(resp) != "application/json") {
         stop("API did not return json", call. = FALSE)
     }
-    jsonlite::fromJSON(content(resp, "text"))
+    parsed <- jsonlite::fromJSON(content(resp, "text"))
+
+    structure(
+      list(
+        content = parsed,
+        path = path,
+        response = resp
+      ),
+      class = "api_post"
+    )
 }
 
+print.api_post <- function(x, ...) {
+  cat("<TFS post", x$path, ">\n", sep = "")
+  str(x$content)
+  invisible(x)
+}
 
